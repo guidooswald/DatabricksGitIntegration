4224ee5f801a1f9371a275e2b5bc76de897cf7e1
Guido Oswald <guido.oswald@gmail.com>
2020-06-25 09:48:12 +0200

    created branch (guido)

--- notebooks/Users/guido.oswald@databricks.com/GuidoGitIntegration.py
+++ notebooks/Users/guido.oswald@databricks.com/GuidoGitIntegration.py
@@ -1,2 +1,3 @@
 # Databricks notebook source
+# This is a Databricks Notebook synced with a public GitHub repository at https://github.com/guidooswald/DatabricksGitIntegration
 print("hello GitHub!!")
