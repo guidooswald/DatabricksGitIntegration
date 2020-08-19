44321aed1bb6c9a592e0b84f90e95e092b904709
guidooswald <>
2020-08-19 09:21:48 +0000

    initial load
    v1

---
+++ DatabricksProjectTest.sql
@@ -0,0 +1,4 @@
+-- Databricks notebook source
+SET test.nrows = 1000000;
+SET test.npartitions = 512;
+SELECT * FROM RANGE(0, ${test.nrows}, 1, ${test.npartitions})  -- start, end, step, numPartitions
