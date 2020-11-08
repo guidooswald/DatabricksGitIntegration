fdc9506bff5302e248f29cda80219299a6f71739
guidooswald <>
2020-11-08 21:07:19 +0000

    test ch
    test ch

--- DatabricksProjectTest.sql
+++ DatabricksProjectTest.sql
@@ -1,4 +1,5 @@
 -- Databricks notebook source
+#test test
 SET test.nrows = 1000000;
 SET test.npartitions = 512;
 SELECT * FROM RANGE(0, ${test.nrows}, 1, ${test.npartitions})  -- start, end, step, numPartitions
