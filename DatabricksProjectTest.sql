a6346e87029bcb4120df43d71ef8d0320ab0c807
guidooswald <guidooswald>
2020-12-02 19:22:27 +0000

    fixed 1st cell

--- DatabricksProjectTest.sql
+++ DatabricksProjectTest.sql
@@ -1,12 +1,12 @@
 -- Databricks notebook source
-#test test
+--test test
 SET test.nrows = 1000000;
 SET test.npartitions = 512;
 SELECT * FROM RANGE(0, ${test.nrows}, 1, ${test.npartitions})  -- start, end, step, numPartitions
 
 -- COMMAND ----------
 
-select * from books
+select * from us500 limit 20;
 
 -- COMMAND ----------
 
