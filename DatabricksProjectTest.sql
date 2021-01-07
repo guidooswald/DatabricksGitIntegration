78056587e76c61494816ce7f0c51d4a6c5d0edd3
guidooswald <guidooswald>
2021-01-07 08:09:25 +0000

    update +1

--- DatabricksProjectTest.sql
+++ DatabricksProjectTest.sql
@@ -1,12 +1,12 @@
 -- Databricks notebook source
 --test test
-SET test.nrows = 1000000;
+SET test.nrows = 1000001;
 SET test.npartitions = 512;
 SELECT * FROM RANGE(0, ${test.nrows}, 1, ${test.npartitions})  -- start, end, step, numPartitions
 
 -- COMMAND ----------
 
-select * from us500 limit 20;
+select * from us500 limit 30;
 
 -- COMMAND ----------
 
